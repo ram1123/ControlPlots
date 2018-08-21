caa84370abcf722829bff347aeb67800ba57b57f
ram1123 <ramkrishna.sharma71@gmail.com>
2018-08-21 04:11:01 -0500

    Updated background estimated root files

--- LimitCardGenerate/Utils.py
+++ LimitCardGenerate/Utils.py
@@ -1,13 +1,13 @@
 import ROOT as r
 
-def createRatio(h1, h2, xlabel, col):
+def createRatio(h1, h2, xlabel, col, ymin=0.4, ymax=3.5):
     h3 = h2.Clone("h3")
     h3.SetLineColor(col)
     h3.SetMarkerColor(col)
     h3.SetMarkerStyle(21)
     h3.SetTitle("")
-    h3.SetMinimum(0.5)
-    h3.SetMaximum(3.0)
+    h3.SetMinimum(ymin)
+    h3.SetMaximum(ymax)
     # Set up plot for markers and errors
     #h3.Sumw2()
     h3.SetStats(0)
@@ -34,14 +34,14 @@ def createRatio(h1, h2, xlabel, col):
 
     return h3
 
-def createCanvasPads():
+def createCanvasPads(log1=0, log2=0):
     c = r.TCanvas("c", "canvas", 800, 800)
     # Upper histogram plot is pad1
     pad1 = r.TPad("pad1", "pad1", 0.0, 0.3, 1.0, 1.0)
     pad1.SetBottomMargin(0)  # joins upper and lower plot
     pad1.SetLeftMargin(0.1)
     pad1.SetRightMargin(0.03)
-    #pad1.SetLogy(1)
+    pad1.SetLogy(log1)
     #pad1.SetGridx()
     pad1.SetTickx(1)
     pad1.SetTicky(1)
@@ -49,7 +49,7 @@ def createCanvasPads():
     # Lower ratio plot is pad2
     c.cd()  # returns to main canvas before defining pad2
     pad2 = r.TPad("pad2", "pad2", 0.0, 0.00, 1.0, 0.3)
-    #pad2.SetLogy(1)
+    pad2.SetLogy(log2)
     pad2.SetTopMargin(0)  # joins upper and lower plot
     pad2.SetBottomMargin(0.25)
     pad2.SetLeftMargin(0.1)
